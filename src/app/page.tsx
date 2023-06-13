845b0ec898b49a501a51fa43a182f3b20bdbe3f0
Jared Grimes <jaredlgrimes@hotmail.com>
2023-06-13 16:13:27 -0500

    reworked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,41 +13,50 @@ import {
 export default function Home() {
   return (
     <ChakraProvider>
-      <Container paddingY={8}>
-        <Stack spacing={8}>
+      <Container paddingY={{ base: '16', sm: '32' }}>
+        <Stack spacing={{ base: '4', sm: '6' }}>
+          <Heading size='3xl'>👨🏻‍💻</Heading>
           <Heading size='3xl' color='gray.700'>{`Hi, I'm Jared.`}</Heading>
-          <Grid gridTemplateColumns={{ base: '3.3rem auto', md: '4rem auto' }} alignItems='center' rowGap={{ base: '6', md: '8'}}>
-            <Heading size='xl' color='gray.500'>
-              👨🏻‍💻
-            </Heading>
-            <Heading size='lg' color='gray.500'>
-              This is me fr. I work at{' '}
-              <Link
-                href='https://www.linkedin.com/company/microsoft'
-                isExternal
-              >
-                Microsoft
-              </Link>{' '}
-              making cool front-end for video.
-            </Heading>
-            <Heading size='xl' color='gray.500'>
-              📦
-            </Heading>
-            <Heading size='lg' color='gray.500'>
-              I used to work at{' '}
-              <Link href='https://www.linkedin.com/company/amazon/' isExternal>
-                Amazon
-              </Link>{' '}
-              building financial apps with lots of big data.
-            </Heading>
-            <Heading size='xl' color='gray.500'>
-              📈
-            </Heading>
-            <Heading size='lg' color='gray.500'>
-              I made <Link href='https://pokestats.live' isExternal>pokestats.live</Link>,
-              used by 90,000 Pokémon players and fans.
-            </Heading>
-          </Grid>
+          <Text fontSize='xl' color='gray.600'>
+            {`I'm a front-end engineer `}
+            <Link
+              href='https://www.linkedin.com/company/microsoft'
+              isExternal
+              fontWeight='semibold'
+            >
+              @microsoft
+            </Link>{' '}
+            building front-end and AI solutions for Office Media. Previously{' '}
+            <Link
+              href='https://www.linkedin.com/company/amazon'
+              isExternal
+              fontWeight='semibold'
+            >
+              @amazon.
+            </Link>
+          </Text>
+          <Text fontSize='xl' color='gray.600'>
+            {`I built `}
+            <Link
+              href='https://pokestats.live'
+              isExternal
+              fontWeight='semibold'
+            >
+              pokestats.live
+            </Link>
+            {`, a live tournament data analytics tool used by 100,000+ members of the Pokémon community.`}
+          </Text>
+          <Text fontSize='xl' color='gray.600'>
+            {`Wanna see more? Follow me `}
+            <Link
+              href='https://twitter.com/jgrimesey'
+              isExternal
+              fontWeight='semibold'
+            >
+              @jgrimesey
+            </Link>
+            .
+          </Text>
         </Stack>
       </Container>
     </ChakraProvider>
