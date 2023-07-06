d74d446c7bc5ad0395ebd142da5d33496681fd2f
Jared Grimes <jaredlgrimes@hotmail.com>
2023-07-06 15:20:42 -0500

    more things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import {
   ChakraProvider,
   Container,
   Grid,
+  HStack,
   Heading,
+  Icon,
   Link,
   Stack,
   Text,
 } from '@chakra-ui/react';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
 
 export default function Home() {
   return (
@@ -26,17 +29,21 @@ export default function Home() {
             >
               @microsoft
             </Link>{' '}
-            building front-end and AI solutions for Office Media. Previously{' '}
+            building front-end and AI-powered enterprise solutions for Office Media.
+          </Text>
+          <Text fontSize='xl' color='gray.600'>
+            {`I worked for  `}
             <Link
               href='https://www.linkedin.com/company/amazon'
               isExternal
               fontWeight='semibold'
             >
-              @amazon.
+              @amazon
             </Link>
+            {` on business applications and introduced a plethora of new technologies to our stack.`}
           </Text>
           <Text fontSize='xl' color='gray.600'>
-            {`I built `}
+            {`I made `}
             <Link
               href='https://pokestats.live'
               isExternal
@@ -46,17 +53,49 @@ export default function Home() {
             </Link>
             {`, a live tournament data analytics tool used by 100,000+ members of the Pokémon community.`}
           </Text>
-          <Text fontSize='xl' color='gray.600'>
-            {`Wanna see more? Follow me `}
+            <Text fontSize='xl' color='gray.600'>
+            {`I led development for `}
             <Link
-              href='https://twitter.com/jgrimesey'
+              href='https://www.michigan.gov/coronavirus/news/2020/05/29/mi-symptoms-web-application-helps-michiganders-track-symptoms-informs-reopening-strategy'
               isExternal
               fontWeight='semibold'
             >
-              @jgrimesey
+              MI Symptoms
             </Link>
-            .
+            {`, a COVID symptom tracker for the State of Michigan used 3M+ times.`}
           </Text>
+          <HStack spacing={4}>
+            <HStack>
+              <Icon as={FaTwitter} color='twitter.500' />
+              <Link
+                href='https://twitter.com/jgrimesey'
+                isExternal
+                fontWeight='semibold'
+              >
+                @jgrimesey
+              </Link>
+            </HStack>
+            <HStack spacing={1}>
+              <Text>🧵</Text>
+              <Link
+                href='https://threads.net/@jaredgrimesey'
+                isExternal
+                fontWeight='semibold'
+              >
+                @jaredgrimesey
+              </Link>
+            </HStack>
+            <HStack>
+              <Icon as={FaGithub} color='gray.800' />
+              <Link
+                href='https://github.com/jlgrimes/'
+                isExternal
+                fontWeight='semibold'
+              >
+                jlgrimes
+              </Link>
+            </HStack>
+          </HStack>
         </Stack>
       </Container>
     </ChakraProvider>
